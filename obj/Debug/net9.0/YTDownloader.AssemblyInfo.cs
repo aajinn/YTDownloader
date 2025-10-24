d6acbe8850fc3df72a54bb4f63b5c8c96a26a490
ARed99 <chandyajin@gmail.com>
2025-10-24 10:20:13 +0530

    update gitignore

--- obj/Debug/net9.0/YTDownloader.AssemblyInfo.cs
+++ obj/Debug/net9.0/YTDownloader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YTDownloader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3581277f5f7f35aebea8317acf0d32e8db5171fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("YTDownloader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YTDownloader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
